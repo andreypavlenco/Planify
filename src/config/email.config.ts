74123ae7f7ad323845de7c0f85ea6767979562ed
andreypavlenco <and.pavel.java@gmail.com>
2025-02-02 18:09:25 +0200

    feat: add prometheus.yml

--- src/config/email.config.ts
+++ src/config/email.config.ts
@@ -7,16 +7,16 @@ export const EmailConfig = async (
 ): Promise<MailerOptions> => {
   return {
     transport: {
-      host: configService.get<string>('MAIL_HOST'),
-      port: configService.get<number>('MAIL_PORT'),
+      host: configService.get<string>('EMAIL_HOST'),
+      port: configService.get<number>('EMAIL_PORT'),
       secure: false,
       auth: {
-        user: configService.get<string>('MAIL_USER'),
-        pass: configService.get<string>('MAIL_PASS'),
+        user: configService.get<string>('EMAIL_USER'),
+        pass: configService.get<string>('EMAIL_PASS'),
       },
     },
     defaults: {
-      from: `"${configService.get<string>('MAIL_FROM_NAME')}" <${configService.get<string>('MAIL_FROM_ADDRESS')}>`,
+      from: `"${configService.get<string>('EMAIL_FROM_NAME')}" <${configService.get<string>('MAIL_FROM_ADDRESS')}>`,
     },
     template: {
       dir: process.cwd() + '/src/email/templates/',
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './shared/filters/http-exception.filter';
+import { expressMiddleware as prometheusMiddleware } from 'prometheus-api-metrics';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
+
+const APP_NAME_PREFIX = 'app-name-prefix';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
+
+  app.use(
+    prometheusMiddleware({
+      additionalLabels: ['app'],
+      extractAdditionalLabelValuesFn: () => ({ app: APP_NAME_PREFIX }),
+    }),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('Planify')
@@ -18,7 +28,7 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, documentFactory);
 
   const configService = app.get(ConfigService);
-  const port = configService.get('PORT');
+  const port = configService.get('APP_PORT');
 
   await app.listen(port);
 }
