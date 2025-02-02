75172be855864002ef6ad0e1e83bf4615e4ba061
andreypavlenco <and.pavel.java@gmail.com>
2025-02-02 08:24:56 +0200

    feat: add connect Bullmq

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -2,10 +2,21 @@ import { Global, Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { RedisClient } from './redis.client';
 import { RedisService } from '../core/services/redis.service';
+import { BullModule } from '@nestjs/bullmq';
+
 
 @Global()
 @Module({
+  imports: [
+    BullmqModule.forRootAsync({
+      imports: [],
+      inject: [RedisClient],
+      useFactory: (redisClient: RedisClient) => ({
+        connection: redisClient.getClient(),
+      }),
+    }),
+  ],
   providers: [RedisService, RedisClient, ConfigService],
-  exports: [RedisService],
+  exports: [RedisService, RedisClient],
 })
 export class RedisModule {}
